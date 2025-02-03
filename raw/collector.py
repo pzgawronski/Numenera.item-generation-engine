33055f256967e2058fcfb4f9c5b476d0aadaf93b
Patryk Gawronski <pzgawronski@gmail.com>
2025-02-03 16:16:30 +0100

    added Cypher and Oddity inits + function documentation

--- raw/collector.py
+++ raw/collector.py
@@ -1,28 +1,47 @@
 import csv
 
 
-def collect(source_file, target_dict, collection_class):
+def collect(source_file, collection_class):
+
+    """
+    :param source_file: item list converted from google docs into comma-separated values
+    :param collection_class: item type (Artifact, Cypher, or Oddity)
+    :return new_item_dict: item list converted from csv into python-based dict; index-based
+    """
+
     with open(source_file, newline='', encoding="utf-8") as source:
         new_reader = csv.reader(source, delimiter=",")
         rows_listed = []
         for row in new_reader:
             rows_listed.append(row)
 
+        new_item_dict = {}
+
         for row in rows_listed[1:]:
             new_item = collection_class(row)
-            target_dict[new_item.index] = new_item
+            new_item_dict[new_item.index] = new_item
+
+        return new_item_dict
 
 
 class Numenera:
 
-    def _get_index(self, hundreds: str, tens: str):
+    def _get_index(self, csv_row: list):
+
+        """
+        :param csv_row: single row of comma-separated values containing an item
+        :return: item index starting from 101
+        """
+
+        hundreds = csv_row[0]
+        tens = csv_row[1]
         return int(hundreds) * 100 + int(tens)
 
 
 class Artifact(Numenera):
 
     def __init__(self, csv_row: list):
-        self.index = self._get_index(csv_row[0], csv_row[1])
+        self.index = self._get_index(csv_row)
         self.name = csv_row[2]
         self.description = csv_row[3]
         self.level = csv_row[4]
@@ -31,8 +50,17 @@ class Artifact(Numenera):
 
 
 class Cypher(Numenera):
-    pass
+
+    def __init__(self, csv_row: list):
+        self.index = self._get_index(csv_row)
+        self.name = csv_row[2]
+        self.description = csv_row[3]
+        self.level = csv_row[4]
+        self.form = csv_row[5:]
 
 
 class Oddity(Numenera):
-    pass
+
+    def __init__(self, csv_row: list):
+        self.index = self._get_index(csv_row)
+        self.description = csv_row[2]
--- raw/csv_parser.py
+++ raw/csv_parser.py
@@ -1,14 +1,14 @@
 import csv
-from collector import Artifact, collect
+from collector import Artifact, Cypher, Oddity, collect
 
 raw_artifacts = "./RAW_ITEMS_ARTIFACTS.csv"
 raw_cyphers = "./RAW_ITEMS_CYPHERS.csv"
 raw_oddities = "./RAW_ITEMS_ODDITIES.csv"
 
-ARTIFACTS = {}
-CYPHERS = {}
-ODDITIES = {}
+ARTIFACTS = collect(raw_artifacts, Artifact)
+CYPHERS = collect(raw_cyphers, Cypher)
+ODDITIES = collect(raw_oddities, Oddity)
 
-collect(raw_artifacts, ARTIFACTS, Artifact)
-
-print(ARTIFACTS)
+print(len(ARTIFACTS))
+print(len(CYPHERS))
+print(len(ODDITIES))
