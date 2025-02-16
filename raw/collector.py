2ab281850c8b6e2a8ead48cdadb6e01492bbef05
Zee <pzgawronski@gmail.com>
2025-02-16 11:46:43 +0100

    added __str__ methods for all item classes

--- raw/collector.py
+++ raw/collector.py
@@ -5,9 +5,9 @@ class Collector:
 
     def __init__(self, source_file: str, collection_class):
         """
-        :param source_file: item list converted from google docs into comma-separated values
+        :param source_file: item list converted from Google Docs into comma-separated values
         :param collection_class: item type (Artifact, Cypher, or Oddity)
-        :return new_item_dict: item list converted from csv into python-based dict; index-based
+        :return new_item_dict: item list converted from csv into Python-based dict; index-based
         """
 
         with open(source_file, newline='', encoding="utf-8") as source:
@@ -21,45 +21,3 @@ class Collector:
             for row in rows_listed[1:]:
                 new_item = collection_class(row)
                 self.collection[new_item.index] = new_item
-
-
-class Numenera:
-
-    def _get_index(self, csv_row: list):
-
-        """
-        :param csv_row: single row of comma-separated values containing an item
-        :return: item index starting from 101
-        """
-
-        hundreds = csv_row[0]
-        tens = csv_row[1]
-        return int(hundreds) * 100 + int(tens)
-
-
-class Artifact(Numenera):
-
-    def __init__(self, csv_row: list):
-        self.index = self._get_index(csv_row)
-        self.name = csv_row[2]
-        self.description = csv_row[3]
-        self.level = csv_row[4]
-        self.depletion = csv_row[5]
-        self.form = csv_row[6:]
-
-
-class Cypher(Numenera):
-
-    def __init__(self, csv_row: list):
-        self.index = self._get_index(csv_row)
-        self.name = csv_row[2]
-        self.description = csv_row[3]
-        self.level = csv_row[4]
-        self.form = csv_row[5:]
-
-
-class Oddity(Numenera):
-
-    def __init__(self, csv_row: list):
-        self.index = self._get_index(csv_row)
-        self.description = csv_row[2]
